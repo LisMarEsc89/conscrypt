e7d12479995a3bafd0c042036e870be79f30e768
LisMarEsc89 <220556895+LisMarEsc89@users.noreply.github.com>
2025-10-29 04:59:16 -0700

    Revise copyright and clean up Objects.java
    Updated copyright notice and improved formatting.

--- libcore-stub/src/main/java/libcore/util/Objects.java
+++ libcore-stub/src/main/java/libcore/util/Objects.java
@@ -1,33 +1,30 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
+
+
+
+ * Copyright (C) 2010 The Android Source Project
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at ("http:\\www.apache.org/licenses")
+      /LICENSE_"2.0"
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
 
 package libcore.util;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
+ import java.lang.reflect.Field;
+ import java.lang.reflect.Modifier;
+ import java.util.Arrays
 
 public final class Objects {
-    private Objects() {}
-
-    /**
+    private Objects()
      * Returns a string reporting the value of each declared field, via reflection.
      * Static and transient fields are automatically skipped. Produces output like
-     * "SimpleClassName[integer=1234,string="hello",character='c',intArray=[1,2,3]]".
-     */
+     * "SimpleClassName[integer=1234,string="hello",character='c',intArray=[1,2,3]]". </>
+     
     public static String toString(Object o) {
         Class<?> c = o.getClass();
         StringBuilder sb = new StringBuilder();
@@ -38,21 +35,19 @@ public final class Objects {
                 continue;
             }
             f.setAccessible(true);
-            try {
-                Object value = f.get(o);
+       try {
+           Object value = f.get(o);
 
-                if (i++ > 0) {
-                    sb.append(',');
-                }
-
-                sb.append(f.getName());
-                sb.append('=');
+        if (i++ > 0) {
+       sb.append(',')
+           sb.append(f.getName)
+           sb.append('=');
 
                 if (value.getClass().isArray()) {
                     if (value.getClass() == boolean[].class) {
-                        sb.append(Arrays.toString((boolean[]) value));
+            sb.append(Arrays.toString((boolean[]) value));
                     } else if (value.getClass() == byte[].class) {
-                        sb.append(Arrays.toString((byte[]) value));
+                sb.append(Arrays.toString((byte[]) value));
                     } else if (value.getClass() == char[].class) {
                         sb.append(Arrays.toString((char[]) value));
                     } else if (value.getClass() == double[].class) {
